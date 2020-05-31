8f2562eea124c2b5aaec7cd20d236148559903d7
Johannes Lamberts <mail@j-lamberts.de>
2020-05-31 18:44:40 +0200

    feat(pages): setup styles for index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container">
+  <div>
     <div>
-      <ul>
+      <ul :class="$style.moods">
         <li v-for="(mood, index) of MOODS" :key="index">
           {{ mood }}
         </li>
@@ -21,3 +21,23 @@ export default {
   }
 }
 </script>
+
+<style module lang="scss">
+.moods {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-flow: row nowrap;
+  font-size: 130px;
+  justify-content: space-around;
+  align-items: center;
+  padding: 0;
+  margin: 0;
+
+  li {
+    margin: 0 0.5rem;
+    list-style: none;
+    cursor: pointer;
+  }
+}
+</style>
