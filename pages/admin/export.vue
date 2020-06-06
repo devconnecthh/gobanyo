615db414defb79e9fedf74ee22b4c6e4b66d6301
Johannes Lamberts <mail@j-lamberts.de>
2020-06-06 15:38:53 +0200

    feat: use web api for export

--- pages/admin/export.vue
+++ pages/admin/export.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>{{ dbContent }}</div>
+  <div>
+    <b-btn v-if="canShare" @click="share">Share</b-btn>
+    <pre>{{ dbContent }}</pre>
+  </div>
 </template>
 <script>
 import { get, keys } from 'idb-keyval'
@@ -22,14 +25,22 @@ async function getDbContent() {
 
 export default {
   data() {
-    return { dbContent: {} }
+    return { dbContent: {}, canShare: false }
   },
   mounted() {
     this.loadContent()
+    this.canShare = typeof navigator !== 'undefined' && 'share' in navigator
   },
   methods: {
     async loadContent() {
       this.dbContent = await getDbContent()
+    },
+    async share() {
+      const shareData = {
+        title: `Result exports ${new Date().toISOString()}`,
+        text: JSON.stringify(this.dbContent, null, 2)
+      }
+      await navigator.share(shareData).catch(() => null)
     }
   }
 }
