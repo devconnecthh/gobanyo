27fc49cc2b71a4982a10d3ba7e619207c4cdceba
Johannes Lamberts <mail@j-lamberts.de>
2020-06-06 15:49:43 +0200

    feat: setup navbar

---
+++ components/layout/navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <b-navbar>
+    <b-navbar-nav>
+      <b-nav-item to="/">Home</b-nav-item>
+      <b-nav-item to="/survey">Survey</b-nav-item>
+      <b-nav-item to="/admin/export">Export</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
+    <NavBar />
     <nuxt />
   </div>
 </template>
+
+<script>
+import NavBar from '~/components/layout/navbar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style lang="scss">
 @import '~/assets/styles/base.scss';
