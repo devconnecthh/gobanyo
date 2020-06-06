44db2ad121e6ee0361c68103d036bfb6dec3a872
Johannes Lamberts <mail@j-lamberts.de>
2020-06-06 13:55:06 +0200

    feat: setup initial survey page
    Co-authored-by: Abdelrahman Rifai <abdelrahman.rifai@gmail.com>
    Co-authored-by: valeriarosenberg <valeria.rosenberg@gmx.de>

---
+++ pages/survey.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    Survey
+    <button @click="good">Good</button>
+    <button @click="bad">Bad</button>
+    <p>Satisfaction: {{ satisfaction }}</p>
+    <button v-if="satisfaction" @click="complete">Complete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SurveyPage',
+  data() {
+    return {
+      satisfaction: null
+    }
+  },
+  methods: {
+    good() {
+      this.satisfaction = 'good'
+    },
+    bad() {
+      this.satisfaction = 'bad'
+    },
+    complete() {
+      console.log('Done: ', this.satisfaction)
+    }
+  }
+}
+</script>
