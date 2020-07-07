e63d2f4e827773d916d1fd83c336a18ace948cf5
Valeria Rosenberg <vr@cargonexx.de>
2020-07-07 19:14:15 +0200

    feat: Add input fields and submit button

---
+++ pages/admin/changePin.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-form>
+      <b-input
+        v-model="oldPin"
+        inputmode="numeric"
+        placeholder="Old pin"
+        type="password"
+        autofocus
+      />
+      <b-input
+        v-model="newPin"
+        inputmode="numeric"
+        placeholder="New pin"
+        type="password"
+      />
+    </b-form>
+    <b-btn type="submit">Submit</b-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      oldPin: '',
+      newPin: ''
+    }
+  }
+}
+</script>
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,7 +1,8 @@
 <template>
   <main>
     <h1>Administration</h1>
-    <nuxt-link to="/admin/export">Export</nuxt-link>
+    <nuxt-link to="/admin/export">Export</nuxt-link> <br />
+    <nuxt-link to="/admin/changePin">Change Pin</nuxt-link>
   </main>
 </template>
 
